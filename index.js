fe64a3d467805360afae6a752e60e0b5337161a6
X1vova1X <128622578+X1vova1X@users.noreply.github.com>
2025-01-03 22:18:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,13 +24,14 @@ app.get('/post', async (req, res) => {
 
 app.get('/connor/send', async (req, res) => {
   let { content, name } = req.query;
+  content = `Name: \`${name}\`. Content: \`${name}\``
   if (name == null) {
     name = "No name";
   }
   try {
     const response = await axios.get("https://raw.githubusercontent.com/X1vova1X/easygame/refs/heads/main/games/wbhk");
     const url = response.data;
-    await axios.post(url, { `Name: \`${name}\`. Content: \`${name}\`` });
+    await axios.post(url, { content });
     res.status(200).json({ message: 'Message sent successfully' });
   } catch (error) {
     console.error('Error sending message:', error);
