e5cf02ffa7e55a2c775e7358fb18d8bd0cfb151f
X1vova1X <128622578+X1vova1X@users.noreply.github.com>
2025-01-03 22:15:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ app.get('/post', async (req, res) => {
   }
 });
 
+app.get('/connor/send', async (req, res) => {
+  const { content } = req.query;
+  try {
+    const response = await axios.get("https://raw.githubusercontent.com/X1vova1X/easygame/refs/heads/main/games/wbhk");
+    const url = response.data;
+    await axios.post(url, { content });
+    res.status(200).json({ message: 'Message sent successfully' });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).json({ error: 'Failed to send message' });
+  }
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
